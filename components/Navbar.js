05f41d32294d2a7374174a8b167191d016378cce
Richi Thampan <richithampan@gmail.com>
2020-05-11 11:02:23 -0400

    folder rename

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar(props){
     return(
         <div>
             <nav>
-
+                <img src="../url-shortening" />
             </nav>
             <style jsx>{`
             
