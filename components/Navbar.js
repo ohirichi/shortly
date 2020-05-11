9418e6e0f2d63127dc1189f44b5cf677608f421f
Richi Thampan <richithampan@gmail.com>
2020-05-11 12:04:27 -0400

    moving assets to public folder

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,40 @@
 import Button from "./Button"
+// import  test from "../assets/images/logo"
 
 export default function Navbar(props){
 
     return(
         <div>
             <nav>
-                <img src="../url-shortening" />
+                <img className="logo" src="/images/logo.svg" alt="shortly" />
+                <div className="burger">
+                    <div className="hamburger-line"></div>
+                    <div className="hamburger-line"></div>
+                    <div className="hamburger-line"></div>
+                </div>
+                <div className="nav-links">
+                    <ul>
+                        <div>
+                            <li><a href="#">Features</a></li>
+                            <li><a href="#">Pricing</a></li>
+                            <li><a href="#">Resources</a></li>
+                        </div>
+                        <div>
+                            <li><a href="#">Login</a></li>
+                            <Button text="Sign Up" size="sm" />
+                        </div>
+                    </ul>
+
+                </div>
             </nav>
             <style jsx>{`
-            
+                ul{
+                    list-style:none;
+                    
+                }
+                a {
+                    text-decoration:none;
+                }
             `}
             </style>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head"
 import Button from "../components/Button"
+import Navbar from "../components/Navbar"
 
 
 export default function Home(){
@@ -12,6 +13,7 @@ export default function Home(){
             <Head>
                 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;700&display=swap" rel="stylesheet"/> 
             </Head>
+            <Navbar />
             <Button shape="circle" text="Sign Up" size="sm" activeText="Signed Up!" onClick={handleClick} />
             <Button shape="circle" text="Get Started" primary />
         </div>
